882091ddd3043626f9e0b0323798fac161f2cfb4
tuanle273 <tuanle273@gmail.com>
2021-12-25 01:28:34 +0700

    add coursecontroller

---
+++ Appdev/Models/CourseCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Appdev.Models
+{
+    public class CourseCategoryViewModel
+    {
+        public Course Course { get; set; }
+        public Category Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
