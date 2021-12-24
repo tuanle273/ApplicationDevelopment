8a2d439c8af02a5e5529a2b366f6fbc6db7bec5e
tuanle273 <tuanle273@gmail.com>
2021-12-24 17:03:38 +0700

    init project

---
+++ Appdev/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Appdev.Startup))]
+namespace Appdev
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
