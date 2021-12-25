58e8ab81a75ac22432872ba8a0d7bbc75f0d838a
tuanle273 <tuanle273@gmail.com>
2021-12-25 12:59:03 +0700

    done overview

--- Appdev/Controllers/OverViewsController.cs
+++ Appdev/Controllers/OverViewsController.cs
@@ -1,18 +1,44 @@
-﻿using System;
+﻿using Appdev.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Appdev.Controllers
 {
     [Authorize(Roles = "Staff")]
     public class OverViewsController : Controller
     {
-        // GET: OverViews
-        public ActionResult Index()
+        private ApplicationDbContext _db;
+        private static int courseId;
+        private void AddErrors(IdentityResult result)
         {
-            return View();
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+        public OverViewsController()
+        {
+            _db = new ApplicationDbContext();
+        }
+        // GET: OverViews
+        public ActionResult SelectCourse()
+        {
+            return View(_db.Courses.Include(c => c.Category).ToList());
+        }
+
+        public ActionResult Overview(int id)
+        {
+            OverViewViewModel viewModel = new OverViewViewModel()
+            {
+                TrainerList = _db.Assigns.Where(c => c.CourseId == id).Include(t=>t.Trainer).ToList(),
+                TraineeList = _db.Enrolls.Where(c => c.CourseId == id).Include(t => t.Trainee).ToList()
+            };
+            return View(viewModel);
         }
     }
 }
---
+++ Appdev/Models/OverViewViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Appdev.Models
+{
+    public class OverViewViewModel
+    {
+        public IEnumerable<Assign> TrainerList { get; set; }
+        public IEnumerable<Enroll> TraineeList { get; set; }
+    }
+}
